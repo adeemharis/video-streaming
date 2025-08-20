84d2312fdbfb6137dd504e3608460dd4de5e74ef
Adeem Haris <adeemharis@gmail.com>
2025-08-20 18:15:22 +0530

    update userroutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bcrypt from "bcryptjs";
 import { auth } from "../middleware/auth.js";
-import { User } from "../models/User.js";
+import User from "../models/User.js";
 import { upload } from "../middleware/upload.js";
 
 const router = express.Router();
